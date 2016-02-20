3399418f4419670501edd39539518d8a9427c123
Tony Baker <anthony.s.baker@gmail.com>
2016-02-20 12:34:39 -0500

    Mocha test setup

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Test Setup', function() {
+  describe('test something', function () {
+    it('it should pass', function () {
+      expect(1+2).to.equal(3);
+    });
+  });
+});
